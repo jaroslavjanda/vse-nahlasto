f8865804843cf8ae1d3a007903ee9506a49cca51
Komanec Krystof <krystof.komanec@avast.com>
2020-10-26 16:59:57 +0100

    PullRequestMergeTest

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -3,7 +3,7 @@ import React from 'react';
 export function HomePage() {
   return (
     <div className="appWrapper">
-      <h1>Home Page Test 3_Krystof</h1>
+      <h1>Home Page Test 3_Krystof TestMergeRequest</h1>
       <p>4IT580 - Project Template_Elisss2_Oli</p>
     </div>
   );
