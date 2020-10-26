78dadba49c3a38e613999188dde02b0a471339e3
elizabeth <elizabeth.yelizarova@gmail.com>
2020-10-26 11:18:22 +0100

    t3

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -4,7 +4,7 @@ export function HomePage() {
   return (
     <div className="appWrapper">
       <h1>Home Page Test 3_Krystof</h1>
-      <p>4IT580 - Project Template_Eli</p>
+      <p>4IT580 - Project Template_Elisss2</p>
     </div>
   );
 }
