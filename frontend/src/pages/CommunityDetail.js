c1861e552921ab2c51afb996f7ad263572790fa7
Jaroslav Janda <janda.jaroslav5@gmail.com>
2020-11-05 22:09:57 +0100

    prettier + comment about seperating file 💪

--- frontend/src/pages/CommunityDetail.js
+++ frontend/src/pages/CommunityDetail.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useQuery, gql } from '@apollo/client';
-import { toast } from 'react-toastify'
+import { toast } from 'react-toastify';
 import { Spinner, Alert, Button } from 'react-bootstrap';
 import { tr } from 'date-fns/locale';
 
@@ -20,12 +20,15 @@ const CUMMUNITY_DETAIL_QUERY = gql`
 `;
 
 export const CommunityDetail = ({ match }) => {
+
+  // Will be seperated to more files, this will be only networking manager
+  // Time 😢
   const communityId = parseInt(match.params.communityId);
   const quacksState = useQuery(CUMMUNITY_DETAIL_QUERY, {
     variables: { communityId },
   });
 
-  const [isMember, setIsMember] = useState(false)
+  const [isMember, setIsMember] = useState(false);
   const [community, setCommunity] = useState({
     name: '',
     tickets: [],
@@ -34,12 +37,7 @@ export const CommunityDetail = ({ match }) => {
   });
   useEffect(() => {
     if (!quacksState.loading) {
-      const {
-        name,
-        tickets,
-        users,
-        closed,
-      } = quacksState.data.community;
+      const { name, tickets, users, closed } = quacksState.data.community;
       setCommunity({
         name,
         tickets,
@@ -59,59 +57,57 @@ export const CommunityDetail = ({ match }) => {
         <div>
           <h1>{community.name}</h1>
           {console.log(community)}
-          {!community.closed && !isMember &&  (
+          {!community.closed && !isMember && (
             <div>
-              <Alert variant={'success'}><div>
-              Welcome in {community.name} community.
-                </div>
+              <Alert variant={'success'}>
+                <div>Welcome in {community.name} community.</div>
                 <div>
-                  <strong>
-                  This community is open for everyone
-                    </strong>
-                  </div></Alert>
-                  <Button variant="primary" onClick={() => {
-                    toast.success("Your are in community")
-                    setIsMember(true)
-                    }}>Join here</Button>
+                  <strong>This community is open for everyone</strong>
+                </div>
+              </Alert>
+              <Button
+                variant="primary"
+                onClick={() => {
+                  toast.success('Your are in community');
+                  setIsMember(true);
+                }}
+              >
+                Join here
+              </Button>
             </div>
           )}
-           {!community.closed && isMember &&  (
+          {!community.closed && isMember && (
             <div>
-              <Alert variant={'success'}><div>
-              Welcome in {community.name} community.
-                </div>
+              <Alert variant={'success'}>
+                <div>Welcome in {community.name} community.</div>
                 <div>
-                  <strong>
-                  This community is open for everyone
-                    </strong>
-                  </div></Alert>
-                  <h5>
-                    Hey bro 👋 <br/>
-                    as a part of community you can see stats <br/>
-                    ⬇️
-                    </h5>
-                    <div>
-                      {console.log(community.users)}
-                      Number of users: {community.users.length}
-                      </div>
-                      <div>
-                      Number of tickets: {community.tickets.length}
-                      </div>
-
-
+                  <strong>This community is open for everyone</strong>
+                </div>
+              </Alert>
+              <h5>
+                Hey bro 👋 <br />
+                as a part of community you can see stats <br />
+                ⬇️
+              </h5>
+              <div>
+                {console.log(community.users)}
+                Number of users: {community.users.length}
+              </div>
+              <div>Number of tickets: {community.tickets.length}</div>
             </div>
           )}
           {community.closed && (
             <div>
-              <Alert variant={'danger'}><div>
-              Community {community.name} require permmision.
-                </div>
-                <div>
-                  </div></Alert>
-                  <Button variant="danger" onClick={() => 
-                    toast.info("Your request was sended")
-                  
-                    }>Ask for permission</Button>
+              <Alert variant={'danger'}>
+                <div>Community {community.name} require permmision.</div>
+                <div></div>
+              </Alert>
+              <Button
+                variant="danger"
+                onClick={() => toast.info('Your request was sended')}
+              >
+                Ask for permission
+              </Button>
             </div>
           )}
         </div>
