df96ebc886c5ccbb0ab9448b12126d8def6f007a
eliyelizarova <elizabeth.yelizarova@gmail.com>
2020-11-06 12:29:29 +0100

    mutation like

--- backend/src/modules/ticket/mutation.js
+++ backend/src/modules/ticket/mutation.js
@@ -27,3 +27,41 @@ export const addTicket = async (
 
   return ticket;
 };
+
+export const addLike= async (
+  _,
+  { ownerId, ticketId },
+  { dbConnection },
+) => {
+  const dbResponseCheck = await dbConnection.query(
+    `SELECT * FROM \`like\`;`,
+  );
+
+  if (dbResponseCheck[0]){
+    const dbResponseDelete = await dbConnection.query(
+      `DELETE FROM \`like\` WHERE user_id=? AND ticket_id=?;`,
+      [ownerId, ticketId,],
+    );
+  }
+  else{
+    const dbResponseInsert = await dbConnection.query(
+      `INSERT INTO \`like\` (user_id, ticket_id)
+      VALUES (?, ?);`,
+      [ownerId, ticketId,],
+    );
+  }
+
+  const ticket = (
+    await dbConnection.query(
+      `SELECT ticket.ticket_id, title, image, ticket.content, ticket.user_id, community_id, 
+      COUNT(like.ticket_id) likes_count, COUNT(comment.ticket_id) comments_count 
+      FROM ticket 
+      LEFT JOIN \`like\` on ticket.ticket_id = like.ticket_id 
+      LEFT JOIN comment on ticket.ticket_id = comment.ticket_id
+      WHERE ticket.ticket_id = ?
+      GROUP BY ticket.ticket_id, title, image, ticket.content, ticket.user_id, community_id`,
+    )
+  )[0];
+
+  return ticket;
+};
