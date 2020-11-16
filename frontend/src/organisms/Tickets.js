2baa163693762734e2206071f467d6fa8e75f6b9
Jaroslav Janda <janda.jaroslav5@gmail.com>
2020-11-16 14:53:21 +0100

    Fix tickets component

--- frontend/src/organisms/Tickets.js
+++ frontend/src/organisms/Tickets.js
@@ -1,1 +1,29 @@
-export function Tickets({ tickets }) { const [LikedRequest, {data}] = useMutation(LIKE_MUTATION); return ( <div style={{ textAlign: 'center' }}> <div> <CardColumns> {tickets.map((item) => ( <CardsTicket key={item.ticket_id} item={item} like={item.likes_count} requestSendLike={LikedRequest} /> ))} </CardColumns> </div> </div> ); }
+import React from 'react';
+import { CardColumns } from 'react-bootstrap';
+import { useMutation, gql } from '@apollo/client';
+
+import { CardsTicket } from 'src/molecules/CardsTicket'
+
+const LIKE_MUTATION = gql`
+  mutation addLike($ownerId: Int!, $ticketId: Int!) {
+    addLike(ownerId: $ownerId, ticketId: $ticketId) {
+      title
+    }
+  }
+`;
+
+export function Tickets({ tickets }) {
+
+    const [LikedRequest, { data }] = useMutation(LIKE_MUTATION);
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <div>
+                <CardColumns>
+                    {tickets.map((item) => (
+                        <CardsTicket key={item.ticket_id} item={item} like={item.likes_count} requestSendLike={LikedRequest} />
+                    ))}
+                </CardColumns>
+            </div>
+        </div>
+    );
+}
