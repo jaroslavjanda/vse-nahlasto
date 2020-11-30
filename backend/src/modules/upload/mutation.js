365913b27b4c629b2ecd7629ab21431d3bad1d5e
eliyelizarova <elizabeth.yelizarova@gmail.com>
2020-11-30 16:37:22 +0100

    updated

--- backend/src/modules/upload/mutation.js
+++ backend/src/modules/upload/mutation.js
@@ -1,16 +1,16 @@
-import fsPromises from 'fs';
+import fsPromises from 'fs/promises';
 import fs from 'fs';
 import path from 'path';
 import { TICKET_UPLOAD_DIR } from '../../constants';
 
 /**
- * Based image upload.
+ * Based on ticketId, deletes ticket. But beforehand checks if user, who is trying to delete ticket, is owner of a community.
 
  * @param args
  * @returns {Promise<*>}
  */
 export const singleUpload = async (_, args, { dbConnection }) => {
-  // await fsPromises.mkdir(TICKET_UPLOAD_DIR, { recursive: true });
+  await fsPromises.mkdir(TICKET_UPLOAD_DIR, { recursive: true });
 
   return args.file.then((file) => {
     const { createReadStream, filename, mimetype } = file;
