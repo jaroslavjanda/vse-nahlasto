6d0eafe1a30d8af1a024dbdf7bd90c35b9462b58
eliyelizarova <elizabeth.yelizarova@gmail.com>
2020-12-29 12:44:21 +0100

    edited

--- frontend/src/atoms/Footer.js
+++ frontend/src/atoms/Footer.js
@@ -7,8 +7,8 @@ export function Footer({ className, error, ...rest }) {
     <footer id="footer">
       <div className="container">
         <div className="col col-md-auto text-center">
-          <small><a href={route.termsOfService()} style={{color: "white"}}>Smluvní podmínky</a></small><br/>
-          <small className="text-muted">©2020 Nahlaš.To</small>
+          <a href={route.termsOfService()}>Smluvní podmínky</a><br/>
+          <a className="text-muted">©2020 Nahlaš.To</a>
         </div>
       </div>
     </footer>
