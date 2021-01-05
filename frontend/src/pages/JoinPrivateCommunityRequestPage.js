4007b466c934d741cfbd8a6cab303e30e767d15f
Martin Jelínek <mt.jelinek@gmail.com>
2021-01-05 01:34:07 +0100

    * JoinPrivateCommunityRequestPage building #1

--- frontend/src/pages/JoinPrivateCommunityRequestPage.js
+++ frontend/src/pages/JoinPrivateCommunityRequestPage.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Button } from 'src/atoms';
 
-export function JoinPrivateCommunityRequestPage() {
+export function JoinPrivateCommunityRequestPage({ match }) {
+
+  const communityId = parseInt(match.params.communityId)
+  const userEmail = match.params.email
+  const code = parseInt(match.params.code)
+
+
+  // TODO 1: overit ze code je stale validni, pokud ne, neukazat stranku s potvrzovacim tlacitkem
+  // TODO 2: admin komunity by mel byt na teto strance automaticky prihlasen
+  // TODO 3: mutace na potvrzeni pozadavku
+  // TODO 4: hezci FE
+
   return (
     <div className="mw6 center">
-      "ahoj ne"
+      <div>
+        Kliknutím na tlačítko níže povrdíte přístup uživateli k Vaší privátní komunitě.
+        ID komunity: { communityId },
+        user email: { userEmail },
+        code: { code }
+      </div>
+      <Button type="submit" className="mt2 mb3">
+        Potvrdit žádost
+      </Button>
     </div>
   );
 }
